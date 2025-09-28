0d267f105dcbece7d6297d007663c4aa85f1b037
Trevor J Taulien <ttaulien@ececomp2.ecn.purdue.edu>
2025-09-28 17:00:46 -0400

    q2 checkpoint

---
+++ apps/q2/producer/producer.c
@@ -0,0 +1,56 @@
+#include "lab2-api.h"
+#include "usertraps.h"
+#include "misc.h"
+
+#include "spawn.h"
+
+void main (int argc, char *argv[])  
+{
+  shared_buffer *buf;      // Used to access buffer in shared memory page
+  uint32 h_mem;            // Handle to the shared memory page
+  sem_t s_procs_completed; // Semaphore to signal the original process that we're done
+  int res;
+  //depot p_depot;           // products that producer gives to consumer
+  depot p_depot = {.index = 0, .nums = "0123456789"};
+
+  if (argc != 3) {
+    Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_t    o_page_mapped_semaphore>\n");
+    Exit();
+  } 
+
+  // Convert the command-line strings into integers for use as handles
+  h_mem = dstrtol(argv[1], NULL, 10); // The "10" means base 10
+  s_procs_completed = dstrtol(argv[2], NULL, 10);
+
+  // Map shared memory page into this process's memory space
+  if ((buf = (shared_buffer *)shmat(h_mem)) == NULL) {
+    Printf("Could not map the virtual address to the memory in "); Printf(argv[0]); Printf(", exiting...\n");
+    Exit();
+  }
+ 
+  // Now print a message to show that everything worked
+  //Printf("spawn_me: This producer is one of the %d processes you created.\n", buf->numprocs);
+  Printf("spawn_me: PID %d is a producer process you created of a total %d.\n", Getpid(), buf->numprocs);
+  //Printf("spawn_me: My PID is %d\n", Getpid());
+
+  // Producer "producing"
+  //p_depot.index = 0;
+  //p_depot.nums[0] = '7';
+  //p_depot = {.index = 0, .nums = "0123456789"};
+  Printf("spawn_me: PID %d has index %d and data %d\n", Getpid(), p_depot.index, dstrtol(p_depot.nums, NULL, 10));
+
+  if((res = lock_acquire(buf->lock)) == SYNC_FAIL){
+    Printf("spawn_me: PID %d could NOT get lock! Res: %d", Getpid(), res);
+  }
+
+  Printf("spawn_me: PID %d got the lock!\n", Getpid());
+  lock_release(buf->lock);
+
+  // Signal the semaphore to tell the original process that we're done
+  Printf("spawn_me: PID %d is complete.\n", Getpid()); 
+  if(sem_signal(s_procs_completed) != SYNC_SUCCESS) { 
+    Printf("Bad semaphore s_procs_completed (%d) in ", s_procs_completed); Printf(argv[0]); Printf(", exiting...\n"); 
+    Exit(); 
+  } 
+
+}
